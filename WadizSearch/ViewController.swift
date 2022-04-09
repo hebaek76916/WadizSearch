99312bb70d8a15b5f5079e786af355146c2bd6e5
hebaek76916 <qorgusdms15@gmail.com>
2022-04-09 13:19:11 +0900

    SearchBar Setting

--- WadizSearch/ViewController.swift
+++ WadizSearch/ViewController.swift
@@ -8,14 +8,25 @@ import SafariServices
 
 class ViewController: UIViewController {
 
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.enablesReturnKeyAutomatically = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "어떤 프로젝트를 찾고 계신가요?"
+        searchBar.barStyle = .black
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
         // 검색 API 호출 예시 코드
         // 확인 후 제거 가능
+        setupUI()
         API.search(keyword: "캠핑")
             .get { result in
-                print(result)
+                dump(result)
             }
     }
 
@@ -25,4 +36,25 @@ class ViewController: UIViewController {
         safariViewController.modalPresentationStyle = .automatic
         present(safariViewController, animated: true, completion: nil)
     }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+}
+
+private extension ViewController {
+    
+    func setupUI() {
+        view.addSubview(searchBar)
+        [
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                               constant: 24),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                               constant: -24),
+            searchBar.heightAnchor.constraint(equalToConstant: 50),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                          constant: 44)
+        ].forEach{ $0.isActive = true }
+    }
+    
 }
